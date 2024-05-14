db09b8994bea01213242d9a63e8e3b53cfece6a9
Vinnie Agriesti <crazychenz@gmail.com>
2024-05-14 14:02:08 -0400

    Add self extraction to crit pkgs sh

--- collector/collect-critical_pkgs.sh
+++ collector/collect-critical_pkgs.sh
@@ -5,8 +5,29 @@ mkdir -p essential_pkgs/debian
 mkdir -p essential_pkgs/docker
 mkdir -p essential_pkgs/github
 
-docker compose up
+#docker compose up
 
 git bundle create essential_pkgs/lab_essential.bundle --all
 
-tar -cf critical_pkgs.tar essential_pkgs
+#tar -cf critical_pkgs.tar essential_pkgs
+#TARFLAG="-J" # xz compression
+TARFLAG="" # no compression
+cat >critical_pkgs_install.sh <<SH_EOF
+#!/bin/sh
+sudo install -m 0777 -d /opt/imports/
+sudo install -m 0777 -d /opt/state/
+cd /opt/imports/
+
+base64 -d <<TAR_EOF | tar $TARFLAG -xf -
+SH_EOF
+
+tar $TARFLAG -cf - essential_pkgs | base64 -w 72 >>critical_pkgs_install.sh
+
+cat >>critical_pkgs_install.sh <<SH_EOF
+TAR_EOF
+
+cd essential_pkgs
+git clone lab_essential.bundle
+SH_EOF
+
+chmod +x critical_pkgs_install.sh
