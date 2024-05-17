955a3348989a2386ff48b6aa4c4e09be6724f14a
Vinnie Agriesti <crazychenz@gmail.com>
2024-05-17 08:23:52 -0400

    Added flux images

--- deployment/import-images.sh
+++ deployment/import-images.sh
@@ -2,7 +2,7 @@
 
 DOCKER_IMG_DIR=/opt/imports/essential_pkgs/docker/
 
-
+# Essential Images
 until k3s ctr images import ${DOCKER_IMG_DIR}alpine_-_latest.dockerimage; do sleep 1; done;
 
 until k3s ctr images import ${DOCKER_IMG_DIR}caddy_-_alpine.dockerimage; do sleep 1; done;
@@ -20,3 +20,17 @@ until k3s ctr images import ${DOCKER_IMG_DIR}git.lab_-_lab_-_dnsmasq_-_initial.d
 until k3s ctr images import ${DOCKER_IMG_DIR}git.lab_-_lab_-_caddy_-_certs_init.dockerimage; do sleep 1; done;
 
 until k3s ctr images import ${DOCKER_IMG_DIR}git.lab_-_lab_-_caddy_-_initial.dockerimage; do sleep 1; done;
+
+# Flux Images
+until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_notification-controller.dockerimage; do sleep 1; done;
+
+until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_kustomize-controller.dockerimage; do sleep 1; done;
+
+until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_helm-controller.dockerimage; do sleep 1; done;
+
+until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_source-controller.dockerimage; do sleep 1; done;
+
+until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_image-automation-controller.dockerimage; do sleep 1; done;
+
+until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_image-reflector-controller.dockerimage; do sleep 1; done;
+
