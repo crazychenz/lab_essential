475998f13b956894e41e7e81d94a2b114fecddf6
Vinnie Agriesti <crazychenz@gmail.com>
2024-05-24 08:29:58 -0400

    Changed embedded tar technique for self extracting script that doesn't segfault on files more than 2GiB

--- collector/collect-essential_pkgs.sh
+++ collector/collect-essential_pkgs.sh
@@ -5,30 +5,23 @@ mkdir -p essential_pkgs/debian
 mkdir -p essential_pkgs/docker
 mkdir -p essential_pkgs/github
 
-#docker compose up
+docker compose up
 
 git bundle create essential_pkgs/lab_essential.bundle --all
 
-#tar -cf critical_pkgs.tar essential_pkgs
-#TARFLAG="-J" # xz compression
-TARFLAG="-z" # gzip compression
+TARFLAG="" # optionally add compression flag (gzip=>-z, bzip2=>-j, xz=>-J)
 cat >essential_pkgs_install.sh <<SH_EOF
 #!/bin/sh
 sudo install -m 0777 -d /opt/imports/
 sudo install -m 0777 -d /opt/state/
 sudo install -m 0777 -d /opt/artifacts/
 cd /opt/imports/
-
-base64 -d <<TAR_EOF | tar $TARFLAG -H pax -xf -
-SH_EOF
-
-tar $TARFLAG -H pax -cf - essential_pkgs | base64 -w 72 >>essential_pkgs_install.sh
-
-cat >>essential_pkgs_install.sh <<SH_EOF
-TAR_EOF
-
+sed '1,10d' \$0 | tar x
 cd essential_pkgs
 git clone lab_essential.bundle
+exit 0
+# Verbatim tar data following this 10th line.
 SH_EOF
-
+tar $TARFLAG -c essential_pkgs >>essential_pkgs_install.sh
 chmod +x essential_pkgs_install.sh
+
