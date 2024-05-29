9640a65d673d5c5e4d0cb82a73e28ce93f0b707e
Vinnie Agriesti <crazychenz@gmail.com>
2024-05-29 05:42:38 -0400

    Some refactors and fixes following real offline test

--- collector/collect-essential_pkgs.sh
+++ collector/collect-essential_pkgs.sh
@@ -8,16 +8,18 @@ mkdir -p essential_pkgs/github
 docker compose up
 
 git bundle create essential_pkgs/lab_essential.bundle --all
+cp ../services/*offline*.sh ./essential_pkgs/
+# TODO: Run a script here to handle bootstrap  #git clone lab_essential.bundle
 
 TARFLAG="" # optionally add compression flag (gzip=>-z, bzip2=>-j, xz=>-J)
 cat >essential_pkgs_install.sh <<SH_EOF
 #!/bin/sh
-sudo install -m 0777 -d /opt/imports/
-sudo install -m 0777 -d /opt/state/
-sudo install -m 0777 -d /opt/artifacts/
+install -m 0777 -d /opt/imports/
+install -m 0777 -d /opt/state/
+install -m 0777 -d /opt/artifacts/
 sed '1,9d' \$0 | tar -C /opt/imports -x
 cd /opt/imports/essential_pkgs
-git clone lab_essential.bundle
+
 exit 0
 # Verbatim tar data following this 9th line.
 SH_EOF
