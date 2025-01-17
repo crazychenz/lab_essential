efea95f533157c7ba2726cf319d73e79dddf30f0
chenz <crazychenz@gmail.com>
2025-01-17 03:31:04 -0500

    Fixed typos

--- collector/collect-essential_pkgs.sh
+++ collector/collect-essential_pkgs.sh
@@ -14,12 +14,12 @@ cp ../services/*offline*.sh ./essential_pkgs/
 TARFLAG="" # optionally add compression flag (gzip=>-z, bzip2=>-j, xz=>-J)
 cat >essential_pkgs_install.sh <<SH_EOF
 #!/bin/sh
+which git >/dev/null || { echo Please install git into PATH. && exit 1; }
 install -m 0777 -d /opt/imports/
 install -m 0777 -d /opt/state/
 install -m 0777 -d /opt/artifacts/
 sed '1,9d' \$0 | tar -C /opt/imports -x
-cd /opt/imports/essential_pkgs
-
+cd /opt/imports/essential_pkgs && git clone lab_essential.bundle
 exit 0
 # Verbatim tar data following this 9th line.
 SH_EOF
