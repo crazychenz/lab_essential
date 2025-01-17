050c3c14bf6a8110466e89f4d9aefbe07d76ab9d
chenz <crazychenz@gmail.com>
2025-01-17 05:59:41 -0500

    Changed image format for services

--- services/build-images.sh
+++ services/build-images.sh
@@ -2,36 +2,24 @@
 
 DOCKER_IMG_DIR=/opt/imports/essential_pkgs/docker/
 
-# Import all of the docker images
-docker load < ${DOCKER_IMG_DIR}alpine_-_latest.dockerimage
+# Import all of the upstream docker images
+docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Falpine%3Alatest.dockerimage
+docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Fcaddy%3Aalpine.dockerimage
+docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Fgitea%3Alatest.dockerimage
+docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Fact_runner%3Alatest.dockerimage
+docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Fvaultwarden%3Alatest.dockerimage
+docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Fnode%3Alts-alpine.dockerimage
+docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Fpostgres%3A14.dockerimage
 
-docker load < ${DOCKER_IMG_DIR}caddy_-_alpine.dockerimage
-#docker tag caddy:alpine git.lab/lab/caddy:alpine
-
-docker load < ${DOCKER_IMG_DIR}postgres_-_14.dockerimage
-#docker tag postgres:14 git.lab/lab/postgres:14
-#docker save -o ${DOCKER_IMG_DIR}git.lab_-_lab_-_postgres_-_14.dockerimage git.lab/lab/postgres:14
-
-docker load < ${DOCKER_IMG_DIR}gitea_-_gitea_-_latest.dockerimage
-#docker tag gitea/gitea:latest git.lab/lab/gitea:latest
-#docker save -o ${DOCKER_IMG_DIR}git.lab_-_lab_-_gitea_-_latest.dockerimage git.lab/lab/gitea:latest
-
+# Build baseline docker images.
 docker compose up -d essential_pkgs_svc
 echo "Waiting 3 seconds for essential package repos to become available."
 sleep 3
-docker compose build dnsmasq_svc
-docker save -o ${DOCKER_IMG_DIR}git.lab_-_lab_-_dnsmasq_-_stage.dockerimage git.lab/lab/dnsmasq:stage
-docker compose build caddy_certs_init
-docker save -o ${DOCKER_IMG_DIR}git.lab_-_lab_-_caddy_certs_init_-_stage.dockerimage git.lab/lab/caddy_certs_init:stage
-docker compose build caddy_rproxy
-docker save -o ${DOCKER_IMG_DIR}git.lab_-_lab_-_caddy_-_stage.dockerimage git.lab/lab/caddy:stage
+docker compose build dnsmasq_svc # builds git.lab/lab/dnsmasq_svc:stage
+docker save -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fdnsmasq_svc%3Astage.dockerimage git.lab/lab/dnsmasq_svc:stage
+docker compose build caddy_certs_init # builds git.lab/lab/caddy_certs_init:stage
+docker save -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_certs_init%3Astage.dockerimage git.lab/lab/caddy_certs_init:stage
+docker compose build caddy_rproxy # builds git.lab/lab/caddy_rproxy:stage
+docker save -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_rproxy%3Astage.dockerimage git.lab/lab/caddy_rproxy:stage
 docker compose down
 
-# TODO: Should these have service builds for completeness?
-# Load and tag images that have no essential builds
-docker load < ${DOCKER_IMG_DIR}vaultwarden_-_server_-_latest.dockerimage
-docker tag vaultwarden/server:latest git.lab/lab/vaultwarden:latest
-docker load < ${DOCKER_IMG_DIR}gitea_-_act_runner_-_latest.dockerimage
-docker tag gitea/act_runner:latest git.lab/lab/gitea_act_runner:latest
-docker load < ${DOCKER_IMG_DIR}node_-_lts-alpine.dockerimage
-docker tag node:lts-alpine git.lab/lab/node:lts-alpine
--- services/push-images.sh
+++ services/push-images.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
-docker push git.lab/lab/caddy:initial
-docker push git.lab/lab/caddy_certs_init:initial
-docker push git.lab/lab/dnsmasq:initial
-docker push git.lab/lab/postgres:14
-docker push git.lab/lab/gitea:latest
-docker push git.lab/lab/gitea_act_runner:latest
+# Out custom images
+docker push git.lab/lab/caddy_rproxy:stage
+docker push git.lab/lab/caddy_certs_init:stage
+docker push git.lab/lab/dnsmasq_svc:stage
+docker push git.lab/lab/gitea_svc:stage
+docker push git.lab/lab/gitea_db:stage
 
-docker push git.lab/lab/caddy:alpine
-docker push git.lab/lab/vaultwarden:latest
-docker push git.lab/lab/node:lts-alpine
+# Upstream images from docker hub
+docker push git.lab/dockerhub/alpine:latest
+docker push git.lab/dockerhub/caddy:alpine
+docker push git.lab/dockerhub/gitea:latest
+docker push git.lab/dockerhub/act_runner:latest
+docker push git.lab/dockerhub/vaultwarden:latest
+docker push git.lab/dockerhub/node:lts-alpine
+docker push git.lab/dockerhub/postgres:14
+
+# Upstream images from ghcr.io
+docker push git.lab/ghcr.io/fluxcd-notification-controller:v1.3.0
+docker push git.lab/ghcr.io/fluxcd-kustomize-controller:v1.3.0
+docker push git.lab/ghcr.io/fluxcd-helm-controller:v1.0.1
+docker push git.lab/ghcr.io/fluxcd-source-controller:v1.3.0
+docker push git.lab/ghcr.io/fluxcd-image-automation-controller:v0.38.0
+docker push git.lab/ghcr.io/fluxcd-image-reflector-controller:v0.32.0
+
+
+
+
 
