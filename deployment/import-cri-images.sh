0d19aa0e613e9bec43764d0295373aa232273ba6
chenz <crazychenz@gmail.com>
2025-01-17 13:26:08 -0500

    Removed flux configs, added dns fixes

--- deployment/import-cri-images.sh
+++ deployment/import-cri-images.sh
@@ -3,31 +3,18 @@
 DOCKER_IMG_DIR=/opt/imports/essential_pkgs/docker/
 
 # Essential Images
-until k3s ctr images import ${DOCKER_IMG_DIR}alpine_-_latest.dockerimage; do sleep 1; done;
 
-until k3s ctr images import ${DOCKER_IMG_DIR}git.lab_-_lab_-_dnsmasq_-_initial.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}git.lab_-_lab_-_caddy_certs_init_-_initial.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}git.lab_-_lab_-_caddy_-_initial.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}git.lab_-_lab_-_gitea_-_latest.dockerimage; do sleep 1; done;
-
-#until k3s ctr images import ${DOCKER_IMG_DIR}caddy_-_alpine.dockerimage; do sleep 1; done;
-#until k3s ctr images import ${DOCKER_IMG_DIR}gitea_-_act_runner_-_latest.dockerimage; do sleep 1; done;
-#until k3s ctr images import ${DOCKER_IMG_DIR}vaultwarden_-_server_-_latest.dockerimage; do sleep 1; done;
-#until k3s ctr images import ${DOCKER_IMG_DIR}node_-_lts-alpine.dockerimage; do sleep 1; done;
+until k3s ctr images import ${DOCKER_IMG_DIR}git.lab%2Flab%2Fdnsmasq_svc%3Astage.dockerimage; do sleep 1; done;
+until k3s ctr images import ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_certs_init%3Astage.dockerimage; do sleep 1; done;
+until k3s ctr images import ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_rproxy%3Astage.dockerimage; do sleep 1; done;
+until k3s ctr images import ${DOCKER_IMG_DIR}git.lab%2Flab%2Fgitea_db%3Astage.dockerimage; do sleep 1; done;
+until k3s ctr images import ${DOCKER_IMG_DIR}git.lab%2Flab%2Fgitea_svc%3Astage.dockerimage; do sleep 1; done;
 
 # Flux Images
-until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_notification-controller.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_kustomize-controller.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_helm-controller.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_source-controller.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_image-automation-controller.dockerimage; do sleep 1; done;
-
-until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_image-reflector-controller.dockerimage; do sleep 1; done;
+# until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_notification-controller.dockerimage; do sleep 1; done;
+# until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_kustomize-controller.dockerimage; do sleep 1; done;
+# until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_helm-controller.dockerimage; do sleep 1; done;
+# until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_source-controller.dockerimage; do sleep 1; done;
+# until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_image-automation-controller.dockerimage; do sleep 1; done;
+# until k3s ctr images import ${DOCKER_IMG_DIR}ghcr.io_-_fluxcd_-_image-reflector-controller.dockerimage; do sleep 1; done;
 
--- services/build-images.sh
+++ services/build-images.sh
@@ -2,6 +2,8 @@
 
 DOCKER_IMG_DIR=/opt/imports/essential_pkgs/docker/
 
+# TODO: Are some of these never customized?
+
 # Import all of the upstream docker images
 docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Falpine%3Alatest.dockerimage
 docker load < ${DOCKER_IMG_DIR}git.lab%2Fdockerhub%2Fcaddy%3Aalpine.dockerimage
@@ -16,10 +18,24 @@ docker compose up -d essential_pkgs_svc
 echo "Waiting 3 seconds for essential package repos to become available."
 sleep 3
 docker compose build dnsmasq_svc # builds git.lab/lab/dnsmasq_svc:stage
-docker save -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fdnsmasq_svc%3Astage.dockerimage git.lab/lab/dnsmasq_svc:stage
+docker save \
+  -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fdnsmasq_svc%3Astage.dockerimage \
+  git.lab/lab/dnsmasq_svc:stage
 docker compose build caddy_certs_init # builds git.lab/lab/caddy_certs_init:stage
-docker save -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_certs_init%3Astage.dockerimage git.lab/lab/caddy_certs_init:stage
+docker save \
+  -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_certs_init%3Astage.dockerimage \
+  git.lab/lab/caddy_certs_init:stage
 docker compose build caddy_rproxy # builds git.lab/lab/caddy_rproxy:stage
-docker save -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_rproxy%3Astage.dockerimage git.lab/lab/caddy_rproxy:stage
+docker save \
+  -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fcaddy_rproxy%3Astage.dockerimage \
+  git.lab/lab/caddy_rproxy:stage
+docker compose build gitea_db # builds git.lab/lab/gitea_db:stage
+docker save \
+  -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fgitea_db%3Astage.dockerimage \
+  git.lab/lab/gitea_db:stage
+docker compose build gitea_svc # builds git.lab/lab/gitea_svc:stage
+docker save \
+  -o ${DOCKER_IMG_DIR}git.lab%2Flab%2Fgitea_svc%3Astage.dockerimage \
+  git.lab/lab/gitea_svc:stage
 docker compose down
 
