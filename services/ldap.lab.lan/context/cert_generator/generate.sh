fa6d23c870d6b9b0faa6d131c0bbd885c1cb9894
CrazyChenz <crazychenz@gmail.com>
2025-01-28 08:29:55 -0500

    Changed ports and removed intermediate cert

--- services/ldap.lab.lan/context/cert_generator/generate.sh
+++ services/ldap.lab.lan/context/cert_generator/generate.sh
@@ -6,22 +6,31 @@ openssl req -newkey rsa:4096 -nodes -keyout lab-root.key -out lab-root.csr -subj
 openssl x509 -req -in lab-root.csr -signkey lab-root.key -out lab-root.crt -days 18250 \
   -extfile <(echo -e "basicConstraints = CA:TRUE\nkeyUsage = keyCertSign, cRLSign")
 
-openssl req -newkey rsa:2048 -nodes -keyout lab-intermediate.key -out lab-intermediate.csr -subj "/CN=IntermediateCA"
+#openssl req -newkey rsa:2048 -nodes -keyout lab-intermediate.key -out lab-intermediate.csr -subj "/CN=IntermediateCA"
 
-openssl x509 -req -in lab-intermediate.csr -CA lab-root.crt -CAkey lab-root.key -CAcreateserial -out lab-intermediate.crt -days 18250 \
-  -extfile <(echo -e "basicConstraints = CA:TRUE, pathlen:0\nkeyUsage = keyCertSign, cRLSign")
+#openssl x509 -req -in lab-intermediate.csr -CA lab-root.crt -CAkey lab-root.key -CAcreateserial -out lab-intermediate.crt -days 18250 \
+#  -extfile <(echo -e "basicConstraints = CA:TRUE, pathlen:0\nkeyUsage = keyCertSign, cRLSign")
 
-openssl req -newkey rsa:2048 -nodes -keyout lab-apps.key -out lab-apps.csr -subj "/CN=lab"
+#openssl req -newkey rsa:2048 -nodes -keyout lab-apps.key -out lab-apps.csr -subj "/CN=lab"
 
-openssl x509 -req -in lab-apps.csr -CA lab-intermediate.crt -CAkey lab-intermediate.key -CAcreateserial -out lab-apps.crt -days 18250 \
+#openssl x509 -req -in lab-apps.csr -CA lab-intermediate.crt -CAkey lab-intermediate.key -CAcreateserial -out lab-apps.crt -days 18250 \
+#  -extfile <(printf "subjectAltName=DNS:dns.lab.lan,DNS:ldap.lab.lan,DNS:git.lab.lan,DNS:words.lab.lan,DNS:*.lab.lan")
+
+#cat lab-intermediate.crt lab-root.crt > lab-root.chain.crt
+#cat lab-apps.crt lab-intermediate.crt lab-root.crt > lab-apps.chain.crt
+
+#openssl verify -CAfile lab-root.crt lab-apps.chain.crt
+
+#openssl verify -CAfile lab-root.crt -untrusted lab-intermediate.crt lab-apps.chain.crt
+
+openssl req -newkey rsa:2048 -nodes -keyout lab.lan.key -out lab.lan.csr -subj "/CN=lab"
+
+openssl x509 -req -in lab.lan.csr -CA lab-root.crt -CAkey lab-root.key -CAcreateserial -out lab.lan.crt -days 18250 \
   -extfile <(printf "subjectAltName=DNS:dns.lab.lan,DNS:ldap.lab.lan,DNS:git.lab.lan,DNS:words.lab.lan,DNS:*.lab.lan")
 
-cat lab-intermediate.crt lab-root.crt > lab-root.chain.crt
-cat lab-apps.crt lab-intermediate.crt lab-root.crt > lab-apps.chain.crt
-
-openssl verify -CAfile lab-root.crt lab-apps.chain.crt
-
-openssl verify -CAfile lab-root.crt -untrusted lab-intermediate.crt lab-apps.chain.crt
+cat lab.lan.crt lab-root.crt > lab.lan.chain.crt
+openssl verify -CAfile lab-root.crt lab.lan.chain.crt
+openssl verify -CAfile lab-root.crt lab.lan.crt
 
 chmod 644 *.crt
 chmod 644 *.key
--- services/ldap.lab.lan/context/client/entrypoint.sh
+++ services/ldap.lab.lan/context/client/entrypoint.sh
@@ -6,9 +6,9 @@
 #/usr/sbin/automount &
 
 cp /certs/lab-root.crt /usr/local/share/ca-certificates/lab-root.crt
-cp /certs/lab-intermediate.crt /usr/local/share/ca-certificates/lab-intermediate.crt
+#cp /certs/lab-intermediate.crt /usr/local/share/ca-certificates/lab-intermediate.crt
 chmod 644 /usr/local/share/ca-certificates/lab-root.crt
-chmod 644 /usr/local/share/ca-certificates/lab-intermediate.crt
+#chmod 644 /usr/local/share/ca-certificates/lab-intermediate.crt
 update-ca-certificates
 
 /bin/bash -i
